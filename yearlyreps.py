bf6c03445accab4eed417aaed95dfb89e2c0b821
Tom Paton <tom.paton@gmail.com>
2025-02-10 12:43:01 +1100

    add post handlers

--- yearlyreps.py
+++ yearlyreps.py
@@ -1,4 +1,5 @@
-from datetime import date
+from datetime import date, datetime
+from django.contrib.auth import authenticate
 from django.db import models
 from django.shortcuts import get_object_or_404, get_list_or_404
 from nanodjango import Django
@@ -7,6 +8,14 @@ app = Django(SQLITE_DATABASE="data/yearlyreps.db")
 app.api.title = "Yearly Reps API Documentation"
 
 from ninja.pagination import paginate  # noqa: E402
+from ninja.security import HttpBasicAuth  # noqa: E402
+
+
+class BasicAuth(HttpBasicAuth):
+    def authenticate(self, request, username, password):
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            return username
 
 
 @app.admin
@@ -31,11 +40,17 @@ class GoalSchema(app.ninja.Schema):
     notes: str
 
 
+class NewGoalSchema(app.ninja.Schema):
+    goal: str
+    target: int
+    notes: str
+
+
 @app.admin
 class Reps(models.Model):
     rep_id = models.AutoField(primary_key=True, blank=False, null=False)
     goal = models.ForeignKey(Goal, models.CASCADE, blank=False, null=False)
-    date = models.IntegerField(blank=False, null=False)
+    date = models.DateField(blank=False, null=False)
     count = models.IntegerField(blank=True, null=False)
     notes = models.TextField(blank=True, null=True)
 
@@ -55,6 +70,12 @@ class RepsSchema(app.ninja.Schema):
     notes: str | None
 
 
+class NewRepSchema(app.ninja.Schema):
+    date: datetime
+    count: int
+    notes: str | None = None
+
+
 @app.api.get("/goals", response=list[GoalSchema])
 def get_goals(request):
     return Goal.objects.all()
@@ -65,7 +86,14 @@ def get_goal(request, goal_id: int):
     return get_object_or_404(Goal, goal_id=goal_id)
 
 
-@app.api.delete("/goals/{int:goal_id}")
+@app.api.post("/goals", auth=BasicAuth(), response=GoalSchema)
+def create_goal(request, new_goal: NewGoalSchema):
+    return Goal.objects.create(
+        goal=new_goal.goal, target=new_goal.target, notes=new_goal.notes
+    )
+
+
+@app.api.delete("/goals/{int:goal_id}", auth=BasicAuth())
 def delete_goal(request, goal_id: int):
     goal = get_object_or_404(Goal, goal_id=goal_id)
     goal.delete()
@@ -83,7 +111,15 @@ def get_rep(request, goal_id: int, rep_id: int):
     return get_object_or_404(Reps, goal_id=goal_id, rep_id=rep_id)
 
 
-@app.api.delete("/goals/{int:goal_id}/reps/{int:rep_id}")
+@app.api.post("/goals/{int:goal_id}/reps", auth=BasicAuth(), response=RepsSchema)
+def create_rep(request, goal_id: int, new_rep: NewRepSchema):
+    goal = get_object_or_404(Goal, goal_id=goal_id)
+    return goal.reps_set.create(
+        date=new_rep.date.date(), count=new_rep.count, notes=new_rep.notes
+    )
+
+
+@app.api.delete("/goals/{int:goal_id}/reps/{int:rep_id}", auth=BasicAuth())
 def delete_rep(request, goal_id: int, rep_id: int):
     rep = get_object_or_404(Reps, goal_id=goal_id, rep_id=rep_id)
     rep.delete()
